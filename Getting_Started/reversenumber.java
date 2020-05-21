0f8df8efa5eaaeea4d8828704f4623b11ebdd1ee
ashish2706 <65677793+ashish2706@users.noreply.github.com>
2020-05-21 17:42:31 +0530

    Update reversenumber.java
    1. You've to display the digits of a number in reverse.
    2. Take as input "n", the number for which digits have to be displayed in reverse.

--- Getting_Started/reversenumber.java
+++ Getting_Started/reversenumber.java
@@ -7,7 +7,6 @@ public class reversenumber {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner s = new Scanner(System.in);
-		
 		System.out.println("Enter the number");
 		
 		int n = s.nextInt();
@@ -16,11 +15,11 @@ public class reversenumber {
 		int rev = 0;
 		while(n>=1){
 			int rem = n % 10;	
-//		 rev = rev*10 + rem;
+		 	rev = rev*10 + rem;
 			System.out.println(rem);
-		 n = n/10;	
+		 	n = n/10;	
 		}
-		System.out.println(rev);
+		System.out.println("Reversed number is "+rev);
 	}
 
 }
