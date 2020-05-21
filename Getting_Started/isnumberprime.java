26ad3e9ee13b34a23d206086dba0a45616513152
ashish2706 <65677793+ashish2706@users.noreply.github.com>
2020-05-21 17:55:45 +0530

    1. You've to check whether a given number is prime or not.   2. Take a number "t" as input representing count of input numbers to be tested.   2. Take a number "n" as input "t" number of times.   3. For each input value of n, print "prime" if the number is prime and "not prime" otherwise.
    1. You've to check whether a given number is prime or not.
    2. Take a number "t" as input representing the count of input numbers to be tested.
    2. Take a number "n" as input "t" number of times.
    3. For each input value of n, print "prime" if the number is prime and "not prime" otherwise.

--- Getting_Started/isnumberprime.java
+++ Getting_Started/isnumberprime.java
@@ -1,44 +1,34 @@
 package Getting_Started;
 import java.util.*;
 
-
 public class isnumberprime {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
 		Scanner s = new Scanner(System.in); 
 		
 		System.out.println("Enter the number of test cases");
 		int t = s.nextInt();
 		
-		while(t>0) { 
-		
+		while(t>0) {
 		System.out.println("Enter the number");
 		int n = s.nextInt();
 		
 		System.out.println();
 		int flag = 0;
 		
-		for(int i = 2;i*i<=n;i++)// kissi bi number ke ek na ek factor root(n) tk aa hi jata hai .agr kisi ka factor root(n) tk nhi
-		{						/// aaya to uske factor aage bi nhi aayenge. so we can directly say ki number is not prime. 
-								// hume number tk loop chalane ki jarurt nhi hai ..
+		for(int i = 2;i*i<=n;i++){// If a given number do not have any factor till root(N), then after that we need not check for 
+		{			///  its factor after that, so we can directly say that number is prime. 
+					// we do not run loop till N or N/2...................
 			if(n%i==0) {
 				flag = 1;
 				break;
-			}
-			
-			
+			}	
 		}
-		
 		if(flag==1)
 			System.out.println("Not prime ");
-		
 		else
 			System.out.println("Prime ");
-		
 		t--;
 		}
 	}
-
 }
