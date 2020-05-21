f00a886c710288afeefee06bfb7fbd37a7f5d7f9
ashish2706 <65677793+ashish2706@users.noreply.github.com>
2020-05-21 17:36:37 +0530

    Update rotateanumber.java
    /*
     You are given two numbers n and k. You are required to rotate n, k times to the right. If k is positive, rotate to the right i.e. remove rightmost
       digit and make it leftmost. Do the reverse for negative value of k. Also k can have an absolute value larger than number of digits in n.
       2. Take as input n and k.
       3. Print the rotated number.
       4. Note - Assume that the number of rotations will not cause leading 0's in the result. e.g. such an input will not be given
       n = 12340056
       k = 3
       r = 05612340
    
    */

--- Getting_Started/rotateanumber.java
+++ Getting_Started/rotateanumber.java
@@ -1,41 +1,40 @@
 package Getting_Started;
 import java.util.*;
 
-
 public class rotateanumber {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner s =  new Scanner(System.in);
 		System.out.println();
-		System.out.println("Enter the number");
+		System.out.println("Enter the Positive Integer");
 		int n = s.nextInt();
 		
-		System.out.println("Enter the K value");
+		System.out.println("Enter the K value for rotation");
 		int k = s.nextInt();
 		
 		int n1 = n,count=0;
-		
 		while(n1>=1) {
 			count++;
 			n1 = n1/10;
 		}
-		System.out.println(count);
-		
-		if(k<0) {
+		System.out.println("Number of digits in the integer = "+count);		
+		while(k<0){
 			k = k+count;
 		}
-		
-		int rev = 0;
+		k = k%count;
+		System.out.println("Absolute K value for rotation = "+ k);
+
+		int rot = 0;
 		while(k>0) {
-			
 			int rem = n%10;
 			int num = n/10;
-			rev = num+ rem*(int)Math.pow(10, count-1);
+			rot = num+ rem*(int)Math.pow(10, count-1);
+			n = rot;
 			k--;
-			n = rev;
 		}
-		System.out.println(rev);
+		System.out.println("Rotated number is = "+rot);
+
 	}
 
 }
