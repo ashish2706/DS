e088b6fe41d2f9a4e35faadfba59846087983eec
ashish2706 <65677793+ashish2706@users.noreply.github.com>
2020-05-22 16:50:31 +0530

    Update Pattern4.java
    ---------------------print the below pattern----------------------
    
    *	*	*	*	*	
    	*	*	*	*	
    		*	*	*	
    			*	*	
    				*

--- patterns/Pattern4.java
+++ patterns/Pattern4.java
@@ -1,21 +1,15 @@
 package patterns;
 import java.util.*;
 
-
 public class Pattern4 {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
 		Scanner s = new Scanner(System.in);
-		
 		System.out.println("Enter the number of rows");
 		int n = s.nextInt();
 		
 		System.out.println();
 		
-		for(int i =1;i<=n;i++)
-		{
+		for(int i =1;i<=n;i++){
 			for(int j =1;j<i;j++)
 				System.out.print("\t");
 			
