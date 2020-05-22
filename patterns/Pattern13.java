65c203326e8b877b0227321edbff746231104ba8
ashish2706 <65677793+ashish2706@users.noreply.github.com>
2020-05-22 17:46:03 +0530

    Update Pattern13.java
    ----------------print the below pattern-------------------
    
    1	
    1	1	
    1	2	1	
    1	3	3	1	
    1	4	6	4	1

--- patterns/Pattern13.java
+++ patterns/Pattern13.java
@@ -1,35 +1,23 @@
 package patterns;
 import java.util.*;
 
-
 public class Pattern13 {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
 		Scanner s = new Scanner(System.in);
-		
 		System.out.println("Enter the number of rows");
-		
 		int n = s.nextInt();
 		System.out.println();
 		
-		for(int i =1;i<=n;i++)
-		{
+		for(int i =1;i<=n;i++){
 			int fact = 1;
-			for(int j =1;j<=i;j++)
-			{ 
+			for(int j =1;j<=i;j++){ 
 			 fact = (factorial(i-1)/(factorial(j-1) * factorial(i-j)));
 				System.out.print(fact+"\t");
 			}
 			System.out.println();
 		}
-		
-
 	}
-	
 	public static int factorial(int a) {
-		
 		int fact= 1;
 		if(a<=0)
 			return 1;
@@ -37,9 +25,8 @@ public class Pattern13 {
 		while(a>=1) {
 			fact =  fact * a;
 			a--;
-		}
+			}
 		}
 		return fact;
 	}
-
 }
