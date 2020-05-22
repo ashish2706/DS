e450cfdb3fc1422202e26d6ef7be87aae5d27424
ashish2706 <65677793+ashish2706@users.noreply.github.com>
2020-05-22 18:48:41 +0530

    Update Pattern18.java
    -------------print the below pattern-----------------
    
    *	*	*	*	*	*	*	
    	*				*	
    		*		*	
    			*	
    		*	*	*	
    	*	*	*	*	*	
    *	*	*	*	*	*	*

--- patterns/Pattern18.java
+++ patterns/Pattern18.java
@@ -2,20 +2,15 @@ package patterns;
 import java.util.*;
 
 public class Pattern18 {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
 		Scanner s = new Scanner(System.in);
 		System.out.println("Enter the number of rows");
-		
 		int n = s.nextInt();
 		
 		int outerspaces = 0;
 		int innerspaces = n-2;
 		
-		for(int i =1;i<=n;i++)
-		{
+		for(int i =1;i<=n;i++){
 			for(int j =1;j<=outerspaces;j++)
 			System.out.print("\t");
 			
@@ -26,30 +21,22 @@ public class Pattern18 {
 				System.out.print("*\t");
 			}
 			
-			else
-			{
+			else{
 				for(int k = 1;k<=innerspaces;k++)
 					System.out.print("\t");
 			}
 			if(i!=n/2+1)
 			System.out.print("*\t");
 			
-			if(i<=n/2)
-			{
+			if(i<=n/2){
 				outerspaces +=1;
-				innerspaces -=2;
-				
+				innerspaces -=2;	
 			}
 			else {
 				outerspaces -=1;
 				innerspaces +=2;
 			}
-			
 			System.out.println();
 		}
-		
-		
-
 	}
-
 }
