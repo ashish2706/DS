bbf6fb422572b02da565ee4bf89879ababc61890
ashish2706 <65677793+ashish2706@users.noreply.github.com>
2020-05-22 16:37:09 +0530

    Update Pattern1.java
    ----------------------Print the below pattern:---------------------------
    *	
    *	*	
    *	*	*	
    *	*	*	*	
    *	*	*	*	*

--- patterns/Pattern1.java
+++ patterns/Pattern1.java
@@ -1,30 +1,18 @@
-
 package patterns;
 import java.util.*;
 
-
 public class Pattern1{
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
 		System.out.println("Enter how many rows to print");
-		
-		// statement required to take input 
-		
 		Scanner s = new Scanner(System.in);
 		int n = s.nextInt();
 		
 		System.out.println(); 
-		
-		for(int i=1;i<=n;i++)
-		{
+		for(int i=1;i<=n;i++){
 			for(int j = 1;j<=i;j++)
-				System.out.print("*\t");
+			System.out.print("*\t");
 			
 			System.out.println();
 		}
 	}
-	
-
 }
