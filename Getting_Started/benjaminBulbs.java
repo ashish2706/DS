2f17b8b6bce97d873d0c0d329c24d56bcf58169b
ashish2706 <65677793+ashish2706@users.noreply.github.com>
2020-05-22 15:40:30 +0530

    Update benjaminBulbs.java
    1. You are given n number of bulbs. They are all switched off. A weird fluctuation in voltage hits the circuit n times. In the 1st fluctuation, all bulbs are toggled, in the 2nd fluctuation every 2nd 
      the bulb is toggled, in the 3rd fluctuation, every 3rd bulb is toggled, and so on. You've to find which bulbs will be switched on after n fluctuations.
      2. Take as input a number n, representing the number of bulbs.
      3. Print all the bulbs that will be on after the nth fluctuation in voltage

--- Getting_Started/benjaminBulbs.java
+++ Getting_Started/benjaminBulbs.java
@@ -1,24 +1,17 @@
 package Getting_Started;
-
 import java.util.Scanner;
 
 public class benjaminBulbs {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
 			Scanner s =  new Scanner(System.in);
 			
 			System.out.println("Enter the number of bulbs");
 			int n = s.nextInt();
 			
 			int fluctuation = n;
-			
-			for(int i =1;i*i<=n;i++)
-			{
+			for(int i =1;i*i<=n;i++){
 				System.out.println(i*i);
-			}
-			
+			}	
 	}
 
 }
