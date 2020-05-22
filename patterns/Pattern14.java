84d2e6bca02ebb1b313dd8ac52bf753a9b8a71c3
ashish2706 <65677793+ashish2706@users.noreply.github.com>
2020-05-22 17:49:14 +0530

    Update Pattern14.java
    ----------------print the pattern---------------
    
    5*1=5
    
    5*2=10
    
    5*3=15
    
    5*4=20
    
    5*5=25
    
    5*6=30
    
    5*7=35
    
    5*8=40
    
    5*9=45
    
    5*10=50

--- patterns/Pattern14.java
+++ patterns/Pattern14.java
@@ -2,23 +2,15 @@ package patterns;
 import java.util.*;
 
 public class Pattern14 {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
 		Scanner s = new Scanner(System.in); 
-		
 		System.out.println("Enter the number");
 		int n = s.nextInt();
 		
 		System.out.println();
-		
 		for(int i =1;i<=10;i++) {
-			
 			System.out.println(n+"*"+i+"="+n*i);
 			System.out.println();
 		}
-
 	}
-
 }
