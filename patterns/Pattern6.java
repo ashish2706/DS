0a0fd711245989e7d06c6cd14db66b1149ad4a75
ashish2706 <65677793+ashish2706@users.noreply.github.com>
2020-05-22 17:08:30 +0530

    Update Pattern6.java
    ----------------print the below pattern-------------------------
    
    *	*	*		*	*	*	
    *	*				*	*	
    *						*	
    *	*				*	*	
    *	*	*		*	*	*

--- patterns/Pattern6.java
+++ patterns/Pattern6.java
@@ -1,27 +1,20 @@
 package patterns;
-
 import java.util.*;
 
 public class Pattern6 {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
 		Scanner s = new Scanner(System.in);
-		System.out.println("Enter the number of rows");
-		
+		System.out.println("Enter the number of rows");	
 		int n = s.nextInt();
-		
+	
 		System.out.println();
 		
 		int stars = n/2 + 1;
 		int spaces = 1;
 		
-		for(int i =1;i<=n;i++)
-		{
+		for(int i =1;i<=n;i++){
 			for(int j=1;j<=stars;j++) 
 				System.out.print("*\t");
-			
 			
 			for(int k = 1;k<=spaces;k++)
 				System.out.print("\t");
@@ -29,21 +22,15 @@ public class Pattern6 {
 			for(int j=1;j<=stars;j++) 
 				System.out.print("*\t");
 			
-			if(i<=n/2)
-			{
+			if(i<=n/2){
 				stars -= 1;
 				spaces += 2; 
 			}
 			else {
-				
 				stars += 1;
 				spaces -= 2;
 			}
-			
 			System.out.println();
-		}
-		
-		
+		}	
 	}
-
 }
