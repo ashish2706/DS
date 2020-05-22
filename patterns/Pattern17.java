c88bd32e414cf8e3c2f489d26759c72c78765a2f
ashish2706 <65677793+ashish2706@users.noreply.github.com>
2020-05-22 18:45:13 +0530

    Update Pattern17.java
    -----------print below pattern-----------------
    
    1								1	
    1	2						2	1	
    1	2	3				3	2	1	
    1	2	3	4		4	3	2	1	
    1	2	3	4	5	4	3	2	1

--- patterns/Pattern17.java
+++ patterns/Pattern17.java
@@ -2,37 +2,27 @@ package patterns;
 import java.util.*;
 
 public class Pattern17 {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
 		Scanner s =  new Scanner(System.in);
 		
 		System.out.println("Enter the number of rows");
 		int n =  s.nextInt();
 		
 		System.out.println();
-		
 		int outerspace = 2,stars = 1;
 		
-		for(int i =1;i<=n;i++)
-		{
+		for(int i =1;i<=n;i++){
 			if(i!=n/2+1) {
 			for(int j =1;j<=outerspace;j++)
 				System.out.print("\t");
 			}
-			
 			else {
 				for(int j =1;j<=outerspace;j++)
 				System.out.print("*\t");
 			}
-
 			
 			for(int k=1;k<=stars;k++)
 				System.out.print("*\t");
-			
-			
-				
 			
 			if(i<=n/2)
 				stars = stars+1;
@@ -41,9 +31,5 @@ public class Pattern17 {
 			
 			System.out.println();
 		}
-		
-		
-
 	}
-
 }
