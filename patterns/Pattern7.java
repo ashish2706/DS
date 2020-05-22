45ed2a4ff5258d77f891a9901bb3a16eaf5e0c06
ashish2706 <65677793+ashish2706@users.noreply.github.com>
2020-05-22 17:11:56 +0530

    Update Pattern7.java
    ----------------print the below pattern---------------
    *					
    	*				
    		*			
    			*		
    				*

--- patterns/Pattern7.java
+++ patterns/Pattern7.java
@@ -1,29 +1,20 @@
 package patterns;
 import java.util.*;
 
-
 public class Pattern7 {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Scanner s = new Scanner(System.in);
-		
 		System.out.println("Enter the number of rows ");
 		int n = s.nextInt();
 		
-		for(int i =1;i<=n;i++)
-		{
-			for(int j = 1;j<=n;j++)
-			{
+		for(int i =1;i<=n;i++){
+			for(int j = 1;j<=n;j++){
 				if(i==j)
 					System.out.print("*\t");
 				else
-					System.out.print("\t");
-				
+					System.out.print("\t");	
 			}
 			System.out.println();
 		}
-		
 	}
-
 }
