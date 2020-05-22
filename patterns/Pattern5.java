fd574fa6b9051a3230f9ef5db47c2b8386130278
ashish2706 <65677793+ashish2706@users.noreply.github.com>
2020-05-22 17:01:05 +0530

    Update Pattern5.java
    ---------------print the below pattern---------------
    
                            *	
    		*	*	*	
    	*	*	*	*	*	
    *	*	*	*	*	*	*	
    	*	*	*	*	*	
    		*	*	*	
    			*

--- patterns/Pattern5.java
+++ patterns/Pattern5.java
@@ -1,45 +1,31 @@
 package patterns;
-
-import java.util.Scanner;
+import java.util.*;
 
 public class Pattern5 {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
 		Scanner s = new Scanner(System.in);
-		
 		System.out.println("Enter the number of rows");
 		int n = s.nextInt();
 		
 		int outerspaces= n/2;
 		int stars =1;
 		
-		for(int i =1;i<=n;i++)
-		{
+		for(int i =1;i<=n;i++){
 			for(int j=1;j<=outerspaces;j++)
 				System.out.print("\t");
 			
 			for(int k =1;k<=stars;k++)
 				System.out.print("*\t");
 			
-//			for(int j=1;j<=outerspaces;j++)
-//				System.out.print("\t");
-			
-			if(i<=n/2)
-			{
+			if(i<=n/2){
 				outerspaces -= 1;
 				stars += 2;
 			}
-			
 			else {
 				outerspaces += 1;
 				stars -= 2;
 			}
-			
 			System.out.println();
-		}
-		
+		}	
 	}
-
 }
