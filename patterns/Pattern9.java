c9f95ca9fcf57d04d8afe1d89f972fa98eb0a8b4
ashish2706 <65677793+ashish2706@users.noreply.github.com>
2020-05-22 17:19:28 +0530

    Update Pattern9.java
    -----------print the below pattern----------
    
    *				*	
    	*		*		
    		*			
    	*		*		
    *				*

--- patterns/Pattern9.java
+++ patterns/Pattern9.java
@@ -2,19 +2,13 @@ package patterns;
 import java.util.*;
 
 public class Pattern9 {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
 		Scanner s = new Scanner(System.in);
 		System.out.println("Enter the number of rows");
-		
 		int n = s.nextInt();
 		
 		for(int i =1;i<=n;i++) {
-			
 			for(int j = 1;j<=n;j++) {
-				
 				if(i+j==n+1 || i == j)
 					System.out.print("*\t");
 				else
@@ -23,5 +17,4 @@ public class Pattern9 {
 			System.out.println();
 		}
 	}
-
 }
